38c82f827badbda5986968d51e7a449f1aa372d6
An-Che Teng <angeldan0627@gmail.com>
2016-11-30 10:30:32 +0800

    Add user's MongoDB Schema

--- backend/model/User.js
+++ backend/model/User.js
@@ -3,6 +3,15 @@ var Schema = mongoose.Schema;
 
 var User = new Schema({
   name: String,
+  unit: String,
+  phone: String,
+  area: String,
+  auth: Number,
+  account: String,
+  pw: String,
+  create_dt: Date,
+  update_dt: Date,
+  delete_dt: Date
 }, {
   versionKey: false
 });
